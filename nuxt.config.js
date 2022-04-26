6a91635b41998fcf8d2de0681882387d50a23889
Megan Blevins <megan.blevins@hearolife.com>
2022-04-25 21:27:22 -0500

    title updated

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'hambey-construction',
+    title: 'Modern Home Builders | Hambey Construction',
     htmlAttrs: {
       lang: 'en'
     },
